edee284b1cc4b5e20356934932469cc8b094c81d
hkomaki <hkomaki@lycorp.co.jp>
2025-07-08 01:08:50 +0900

    認証・ユーザー管理機能（Flask-Login）

---
+++ forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
+class LoginForm(FlaskForm):
+    email = StringField('メールアドレス', validators=[DataRequired(), Email()])
+    password = PasswordField('パスワード', validators=[DataRequired()])
+    submit = SubmitField('ログイン')
+
+class RegisterForm(FlaskForm):
+    username = StringField('ユーザー名', validators=[DataRequired(), Length(min=4, max=25)])
+    email = StringField('メールアドレス', validators=[DataRequired(), Email()])
+    password = PasswordField('パスワード', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('パスワード（確認用）', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('登録')

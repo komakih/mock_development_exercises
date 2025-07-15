901c2f71a062e75c96ff9f10d465c368099fe5bd
hkomaki <hkomaki@lycorp.co.jp>
2025-07-15 11:50:17 +0900

    変更確認のため一時コミット9回目

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 import os
@@ -50,6 +50,10 @@ def create_app():
 
     # エラーハンドラーの登録
     app.register_blueprint(errors_bp)
+
+    @app.errorhandler(403)
+    def forbidden(e):
+        return render_template('errors/403.html'), 403
 
     @app.route('/trigger-error')
     def trigger_error():
